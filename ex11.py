d38defdc01a74fbb696481d95db668f404e7c9f6
leongongola <leongongola@gmail.com>
2023-02-23 14:21:44 -0700

    Update ex11.py

--- ex11.py
+++ ex11.py
@@ -1,7 +1,8 @@
-some_number = 0
-
+11.	A while loop with errors
+Assume a program has been written for the task of adding all integers I = 1, 2, ...10: 
+    
+Some_number = 0
 i = 1
-while i < 11:
-    some_number +=  i
-    i +=  1
-print(some_number)
+While i < 11
+	Some_number += 1
+Print some_number
